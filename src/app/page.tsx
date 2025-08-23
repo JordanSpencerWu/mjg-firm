4a0898290255c506e6341fa1caa8b4390f4a4a1f
Jordan Wu <jordan.spencer.wu@gmail.com>
2025-08-23 17:53:15 -0400

    add gradient animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         loop
         muted
         playsInline
-        className="absolute inset-0 w-full h-full object-cover z-0"
+        className="inset-0 w-full h-screen object-cover z-0"
       >
         <source
           src="/Shebara_90s_16x9_4_K_English_Only_Music_and_Logo_4e51af11f6.mp4"
@@ -16,13 +16,13 @@ export default function Home() {
         Your browser does not support the video tag.
       </video>
 
-      {/* Content Overlay */}
-      <div className="relative z-10 flex items-center justify-center min-h-screen">
-        <div className="text-center text-white">
-          <h1 className="text-6xl font-bold mb-6">Welcome to MJG Firm</h1>
-          <p className="text-xl mb-8">
-            Creating exceptional digital experiences
-          </p>
+      <div className="relative leading-0 gradient-animation">
+        <div className="flex min-h-screen justify-center items-center h-full mx-16">
+          <h2 className="text-2xl text-white md:text-5xl">
+            We create timeless experiences for our guests, balancing modern
+            design with a focus on the present, while upholding hospitality's
+            core values of generosity and compassion to shape lasting memories.{" "}
+          </h2>
         </div>
       </div>
     </div>
