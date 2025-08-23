e72a07015999832d9fd2630bfd7478d6f4e3b7e8
Jordan Wu <jordan.spencer.wu@gmail.com>
2025-08-23 18:05:59 -0400

    add snapping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,37 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+// Register GSAP plugins
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Home() {
+  const gradientSectionRef = useRef(null);
+
+  useEffect(() => {
+    ScrollTrigger.create({
+      trigger: gradientSectionRef.current,
+      start: "top 100px",
+      end: "bottom 100px",
+      snap: {
+        snapTo: 1,
+        duration: 20,
+        ease: "power1.out",
+        inertia: false,
+      },
+      onEnter: () => console.log("Entered gradient section"),
+      onLeave: () => console.log("Left gradient section"),
+      onEnterBack: () => console.log("Entered gradient section from bottom"),
+      onLeaveBack: () => console.log("Left gradient section from top"),
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    };
+  }, []);
+
   return (
     <div className="relative min-h-screen">
       {/* Background Video */}
@@ -16,7 +49,10 @@ export default function Home() {
         Your browser does not support the video tag.
       </video>
 
-      <div className="relative leading-0 gradient-animation">
+      <div
+        ref={gradientSectionRef}
+        className="relative leading-0 gradient-animation"
+      >
         <div className="flex min-h-screen justify-center items-center h-full mx-16">
           <h2 className="text-2xl text-white md:text-5xl">
             We create timeless experiences for our guests, balancing modern
