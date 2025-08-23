f95a2e9de023ef7184f9567ea650cf89ec3a6adb
Jordan Wu <jordan.spencer.wu@gmail.com>
2025-08-23 19:03:14 -0400

    add back scroll trigger to snap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
 "use client";
 
+import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
+  const gradientSectionRef = useRef(null);
+
+  useEffect(() => {
+    ScrollTrigger.create({
+      trigger: gradientSectionRef.current,
+      start: "top 100px",
+      end: "top",
+      snap: {
+        snapTo: [0, 1],
+        duration: 0.3,
+        ease: "power1.out",
+        inertia: false,
+      },
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    };
+  }, []);
   return (
     <div className="relative min-h-screen animate-fade-in">
       {/* Background Video */}
@@ -25,18 +45,22 @@ export default function Home() {
 
       {/* Video Overlay Text */}
       <div className="absolute h-screen inset-0 z-10">
-        <div className="flex flex-col px-8 justify-center md:justify-between md:px-32 md:py-32 h-full">
+        <div className="flex flex-col px-8 justify-center md:justify-between md:px-36 md:py-36 h-full">
           <p className="text-2xl mb-4 md:mb-0 md:text-4xl lg:text-6xl animate-fade-in-up">
             WHERE NOW IS <br /> FOREVER
           </p>
           <p className="text-xl md:text-xl lg:text-2xl animate-fade-in-up-delay">
-            Immerse in the essence of the Red Sea while embracing a <br />
+            Immerse in the essence of the Red Sea while embracing a{" "}
+            <br className="hidden md:block" />
             sustainable future surrounded by breathtaking beauty.
           </p>
         </div>
       </div>
 
-      <div className="relative leading-0 gradient-animation">
+      <div
+        ref={gradientSectionRef}
+        className="relative leading-0 gradient-animation"
+      >
         <div className="flex min-h-screen justify-center items-center h-full mx-16">
           <h2 className="text-2xl text-white md:text-5xl">
             We create timeless experiences for our guests, balancing modern
